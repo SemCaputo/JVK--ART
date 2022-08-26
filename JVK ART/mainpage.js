e5b9162651b151a2ebf31cd420172107111ee70d
SemCaputo <109221534+SemCaputo@users.noreply.github.com>
2022-08-26 12:29:17 +0100

    Add files via upload
    First draft of JVK ART website

---
+++ JVK ART/mainpage.js
@@ -0,0 +1,11 @@
+document.getElementById("gallery").onclick = function () {
+    window.location.href = "gallery.html";
+};
+
+document.getElementById("contact").onclick = function () {
+    window.location.href = "form.html";
+};
+
+document.getElementById("instagram").onclick = function () {
+    window.location.href = "https://www.instagram.com/";
+};
